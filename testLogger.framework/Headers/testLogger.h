00b62302c04c3764055f4501e4fd528cbe41051d
Akash <akash-at-116522020260>
2021-06-04 00:38:53 +0530

    Added testLogger.framework

---
+++ testLogger.framework/Headers/testLogger.h
@@ -0,0 +1,19 @@
+//
+//  testLogger.h
+//  testLogger
+//
+//  Created by Akash on 03/06/21.
+//  Copyright © 2021 Akash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testLogger.
+FOUNDATION_EXPORT double testLoggerVersionNumber;
+
+//! Project version string for testLogger.
+FOUNDATION_EXPORT const unsigned char testLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testLogger/PublicHeader.h>
+
+
